1cac422ef328557e52555203150921ff074c6388
김영주 <kimyj@encar.com>
2021-07-24 21:27:48 +0900

    Movie App init version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
+import Movie from './Movie';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  };
+
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <div>
+        {isLoading 
+          ? 'Loading...' 
+          : movies.map(m => (
+            <Movie
+              key = {m.id}
+              id = {m.id}
+              year = {m.year}
+              title = {m.title}
+              summary = {m.summary}
+              poster = {m.medium_cover_image}
+            />
+         ))}
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    this.getMovies();
+  }
+
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      }
+    } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
+    this.setState({ movies, isLoading: false });
+  }
 }
 
 export default App;
