5478a0a1fa58a0d8ecb75d8c73a20dc85ab4ab53
김영주 <kimyj@encar.com>
2021-07-31 16:33:09 +0900

    add Home component to Route

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.css';
 import { HashRouter, Route } from 'react-router-dom';
 import About from './routes/About';
+import Home from './routes/Home';
 
 function App() {
   return (
     <HashRouter>
+      <Route path="/" exact={true} component={Home} />
       <Route path="/about" component={About} />
     </HashRouter>
   );
