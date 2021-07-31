2a7d91b5d0869dd442d3adb17b9d1d7dafc5cfcc
김영주 <kimyj@encar.com>
2021-07-31 21:27:20 +0900

    add Detail component

---
+++ src/routes/Detail.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+class Detail extends React.Component {
+    componentDidMount() {
+        const { location, history } = this.props;
+        if (location.state === undefined) {
+            history.push('/');
+        }
+    }
+
+    render() {
+        const { location } = this.props;
+        if (location.state === undefined) {
+            return null;
+        } else {
+            console.log(location.state);
+            return (
+                <div>
+                    <h1>{location.state.title}</h1>
+                    <p>{location.state.summary}</p>
+                </div>
+            );
+        }
+    }
+}
+
+export default Detail;
